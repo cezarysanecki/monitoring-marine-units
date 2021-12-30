c5ae1a3df5a57418b080811347f7e184f811fd0d
Cezary Sanecki <devcezz@gmail.com>
2021-12-30 15:02:10 +0100

    Added calling external api

---
+++ src/main/java/pl/devcezz/barentswatch/BarentsWatchExternalApi.java
@@ -0,0 +1,23 @@
+package pl.devcezz.barentswatch;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.QueryParam;
+import java.util.List;
+
+@RegisterRestClient(configKey = "open-positions")
+public interface BarentsWatchExternalApi {
+
+    @GET
+    List<OpenPosition> method(@HeaderParam("Authorization") String token,
+                  @QueryParam(value = "Xmin") double xMin,
+                  @QueryParam(value = "Xmax") double xMax,
+                  @QueryParam(value = "Ymin") double yMin,
+                  @QueryParam(value = "Ymax") double yMax);
+}
+
+record OpenPosition(String timeStamp, Long mmsi, Geometry geometry) {}
+
+record Geometry(String type, List<Double> coordinates) {}
--- src/main/java/pl/devcezz/barentswatch/BarentsWatchResource.java
+++ src/main/java/pl/devcezz/barentswatch/BarentsWatchResource.java
@@ -1,24 +1,31 @@
 package pl.devcezz.barentswatch;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/barentswatch")
 public class BarentsWatchResource {
 
-    private final BarentsWatchService barentsWatchService;
-
-    BarentsWatchResource(BarentsWatchService barentsWatchService) {
-        this.barentsWatchService = barentsWatchService;
-    }
+    @Inject
+    @RestClient
+    BarentsWatchExternalApi barentsWatchExternalApi;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/vessels/positions")
-    public String getVesselsPositionFor(BoundingBox boundingBox) {
-        return barentsWatchService.getVesselsPositionFor(boundingBox);
+    public List<OpenPosition> getVesselsPositionFor(BoundingBox boundingBox) {
+        return barentsWatchExternalApi.method(
+                "Bearer xxx",
+                boundingBox.xMin(),
+                boundingBox.xMax(),
+                boundingBox.yMin(),
+                boundingBox.yMax());
     }
 }
 
