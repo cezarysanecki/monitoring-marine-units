560caa38609d0490b6975790222fa654d7d4b83f
Cezary Sanecki <devcezz@gmail.com>
2022-01-04 10:18:20 +0100

    Ignored some variables of OpenPosition

--- src/main/java/pl/devcezz/barentswatch/externalapi/OpenPosition.java
+++ src/main/java/pl/devcezz/barentswatch/externalapi/OpenPosition.java
@@ -1,9 +1,13 @@
 package pl.devcezz.barentswatch.externalapi;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record OpenPosition(String timeStamp, Integer mmsi, Geometry geometry) {
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     record Geometry(String type, List<Double> coordinates) {
     }
 }
