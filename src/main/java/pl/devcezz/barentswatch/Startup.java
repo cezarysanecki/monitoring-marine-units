b6d4988f7ee5287e596427551d5e4d866549c74f
Cezary Sanecki <devcezz@gmail.com>
2022-01-04 16:01:00 +0100

    Cleaned user vessel repository

--- src/main/java/pl/devcezz/barentswatch/Startup.java
+++ src/main/java/pl/devcezz/barentswatch/Startup.java
@@ -1,7 +1,9 @@
 package pl.devcezz.barentswatch;
 
+import io.quarkus.arc.Priority;
 import io.quarkus.runtime.StartupEvent;
 import pl.devcezz.barentswatch.security.ClientRepository;
+import pl.devcezz.barentswatch.user.UserVesselRepository;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -14,6 +16,15 @@ public class Startup {
     @Inject
     ClientRepository clientRepository;
 
+    @Inject
+    UserVesselRepository userVesselRepository;
+
+    @Priority(value = 100)
+    public void deleteUserVessels(@Observes StartupEvent evt) {
+        userVesselRepository.deleteAll();
+    }
+
+    @Priority(value = 101)
     @Transactional
     public void loadUsers(@Observes StartupEvent evt) {
         clientRepository.deleteAll();
