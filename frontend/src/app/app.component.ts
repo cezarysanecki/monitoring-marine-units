f0ca540924d84248fbc99cba92b6dd46a67321c5
Cezary Sanecki <devcezz@gmail.com>
2022-01-07 00:03:21 +0100

    Moved map to leaflet

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,59 +1,10 @@
-import {Component, ViewEncapsulation} from '@angular/core';
-import {MapsTheme, MapsTooltip, DataLabel, Maps, Marker, ILoadEventArgs} from '@syncfusion/ej2-angular-maps';
-
-Maps.Inject(Marker, MapsTooltip, DataLabel);
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  encapsulation: ViewEncapsulation.None
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // custom code start
-  public load = (args: ILoadEventArgs) => {
-    let theme: string = location.hash.split('/')[1];
-    theme = theme ? theme : 'Material';
-    // @ts-ignore
-    args.maps.theme = <MapsTheme>(theme.charAt(0).toUpperCase() +
-      theme.slice(1)).replace(/-dark/i, 'Dark').replace(/contrast/i, 'Contrast');
-  }
-
-  public zoomSettings: object = {
-    zoomFactor: 7,
-    enable: true
-  }
-
-  public titleSettings: object = {
-    text: 'Headquarters of the United Nations',
-    textStyle: {
-      size: '16px'
-    }
-  };
-
-  public centerPosition: object = {
-    latitude: 40.7209,
-    longitude: -73.9680
-  };
-
-  public layers: object[] = [
-    {
-      layerType: 'OSM',
-      animationDuration: 0,
-      markerSettings: [
-        {
-          visible: true,
-          template: '<div><img src="./assets/maps/images/ballon.png" style="height:30px;width:20px;"></img></div>',
-          dataSource: [{
-            name: 'Manhattan, New York, USA',
-            latitude: 40.7488758,
-            longitude: -73.9730091
-          }],
-          tooltipSettings: {
-            visible: true,
-            valuePath: 'name'
-          }
-        }
-      ]
-    },
-  ];
+  title = 'frontend';
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {
-  DataLabelService,
-  LegendService,
-  MapsComponent,
-  MapsModule,
-  MapsTooltipService
-} from "@syncfusion/ej2-angular-maps";
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  imports: [
-    BrowserModule, MapsModule
-  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
-  bootstrap: [
-    AppComponent
+  imports: [
+    BrowserModule
   ],
-  providers: [
-    MapsComponent,
-    LegendService,
-    DataLabelService,
-    MapsTooltipService
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
