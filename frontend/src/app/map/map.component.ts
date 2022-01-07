7fa87a0e8270d70a7844313fb592ac82b04e3fe1
Cezary Sanecki <devcezz@gmail.com>
2022-01-07 15:21:05 +0100

    Added scaled circles of pupulation

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -45,6 +45,6 @@ export class MapComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.initMap();
-    this.markerService.makeCapitalMarkers(this.map);
+    this.markerService.makeCapitalCircleMarkers(this.map);
   }
 }
--- frontend/src/app/marker.service.ts
+++ frontend/src/app/marker.service.ts
@@ -11,6 +11,10 @@ export class MarkerService {
 
   constructor(private http: HttpClient) { }
 
+  static scaledRadius(val: number, maxVal: number): number {
+    return 20 * (val / maxVal);
+  }
+
   makeCapitalMarkers(map: L.Map): void {
     this.http.get(this.capitals).subscribe((res: any) => {
       for (const c of res.features) {
@@ -21,5 +25,22 @@ export class MarkerService {
         marker.addTo(map);
       }
     })
+  }
+
+  makeCapitalCircleMarkers(map: L.Map): void {
+    this.http.get(this.capitals).subscribe((res: any) => {
+
+      const maxPop = Math.max(...res.features.map((x: any) => x.properties.population), 0);
+
+      for (const c of res.features) {
+        const lon = c.geometry.coordinates[0];
+        const lat = c.geometry.coordinates[1];
+        const circle = L.circleMarker([lat, lon], {
+          radius: MarkerService.scaledRadius(c.properties.population, maxPop)
+        });
+
+        circle.addTo(map);
+      }
+    });
   }
 }
