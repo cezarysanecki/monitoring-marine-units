7f42e1819f02da5be1c107dfedbf89bb221f69c6
Sanecki Cezary <Cezary.Sanecki@snt.pl>
2022-01-11 22:57:42 +0100

    Worked on login page

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent { }
+export class NavbarComponent {}
