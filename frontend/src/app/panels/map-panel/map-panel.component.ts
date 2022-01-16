fb965e1321bf654f3939de0d8770ece812b4c95f
Sanecki Cezary <Cezary.Sanecki@snt.pl>
2022-01-16 21:33:15 +0100

    Added panel for map

---
+++ frontend/src/app/panels/map-panel/map-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './map-panel.component.html',
+  styleUrls: ['./map-panel.component.scss']
+})
+export class MapPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
